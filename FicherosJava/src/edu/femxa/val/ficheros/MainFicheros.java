baa15c102e392448fbf608be83aea1f4d802ecdf
isamaralo <isa-vdm@hotmail.com>
2017-01-11 21:35:53 +0100

    commit

--- FicherosJava/src/edu/femxa/val/ficheros/MainFicheros.java
+++ FicherosJava/src/edu/femxa/val/ficheros/MainFicheros.java
@@ -4,18 +4,18 @@ import java.io.File;
 
 public class MainFicheros {
 
-	public static void listarDirectorio (File directorio)
+	public static void listarDirectorio (String nombre)
 	{
-		String[] lista_ficheros = directorio.list();
-		for(int i = 0; i < lista_ficheros.length; i++)
+		File directorio = new File(nombre);
+		String[] lista_ficheros = null;
+
+		System.out.println(directorio.getName());
+		
+		if(directorio.isDirectory())
 		{
-			File sub_directorio = new File (lista_ficheros[i]);
-			
-			if(sub_directorio.isDirectory())
-				listarDirectorio(sub_directorio);
-			
-			else
-				System.out.println(lista_ficheros[i]);
+			lista_ficheros = directorio.list();
+			for(int i = 0; i < lista_ficheros.length; i++)
+				listarDirectorio(nombre+"\\"+lista_ficheros[i]);
 		}
 	}
 	
@@ -39,37 +39,39 @@ public class MainFicheros {
 		 * Hacer un programa para que desde el directorio actual (“.”), recorra el árbol de
 		 * directorios y muestre todos los directorios, subdirectorios y sus respectivos archivos.
 		 */
-//		File directorio_actual = new File(".");
-//		String[] lista_ficheros_dir_actual = directorio_actual.list();
-//		
-//		if (lista_ficheros_dir_actual != null)
-//			listarDirectorio(directorio_actual);
-//		
-//		else
-//			System.out.println("No hay ficheros en el directorio.");
+		File directorio_actual = new File(".");
+		String[] lista_ficheros_dir_actual = directorio_actual.list();
+		
+		if (lista_ficheros_dir_actual != null)
+		{
+			String nombre = directorio_actual.getName();
+			listarDirectorio(nombre);
+		}
+		else
+			System.out.println("No hay ficheros en el directorio.");
 		
 		/**
 		 * Hacer un programa que dada una ruta, muestre los subdirectorios ocultos.
 		 */
-		File directorios_ocultos = new File ("C:/Users/Administrador.000/IsabelGrupoTarde/FicherosJava");
-		String[] lista_ficheros_ocultos = directorios_ocultos.list();
-		
-		if (lista_ficheros_ocultos != null)
-			for(int i = 0; i < lista_ficheros_ocultos.length; i++)
-			{
-				File sub_directorio = new File (lista_ficheros_ocultos[i]);
-				
-				if((sub_directorio.isDirectory()) && (lista_ficheros_ocultos[i].charAt(0)=='.'))
-				{
-					System.out.println("Ocultos: " +lista_ficheros_ocultos[i]);
-				}
-				else
-					System.out.println("No ocultos: " +lista_ficheros_ocultos[i]);
-			}
-			
-		else
-			System.out.println("No hay ficheros en el directorio.");
-		
+//		File directorios_ocultos = new File ("C:/Users/Administrador.000/IsabelGrupoTarde/FicherosJava");
+//		String[] lista_ficheros_ocultos = directorios_ocultos.list();
+//		
+//		if (lista_ficheros_ocultos != null)
+//			for(int i = 0; i < lista_ficheros_ocultos.length; i++)
+//			{
+//				File sub_directorio = new File (lista_ficheros_ocultos[i]);
+//				
+//				if((sub_directorio.isDirectory()) && (lista_ficheros_ocultos[i].charAt(0)=='.'))
+//				{
+//					System.out.println("Ocultos: " +lista_ficheros_ocultos[i]);
+//				}
+//				else
+//					System.out.println("No ocultos: " +lista_ficheros_ocultos[i]);
+//			}
+//			
+//		else
+//			System.out.println("No hay ficheros ni directorios en el directorio.");
+//		
 		
 		
 	}
