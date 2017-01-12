451969f9de8b51f8cde8e4a60625bbafcd7168a9
isamaralo <isa-vdm@hotmail.com>
2017-01-12 15:53:30 +0100

    commit

--- FicherosJava/src/edu/femxa/val/ficheros/MainFicheros.java
+++ FicherosJava/src/edu/femxa/val/ficheros/MainFicheros.java
@@ -39,39 +39,38 @@ public class MainFicheros {
 		 * Hacer un programa para que desde el directorio actual (“.”), recorra el árbol de
 		 * directorios y muestre todos los directorios, subdirectorios y sus respectivos archivos.
 		 */
-		File directorio_actual = new File(".");
-		String[] lista_ficheros_dir_actual = directorio_actual.list();
-		
-		if (lista_ficheros_dir_actual != null)
-		{
-			String nombre = directorio_actual.getName();
-			listarDirectorio(nombre);
-		}
-		else
-			System.out.println("No hay ficheros en el directorio.");
+//		File directorio_actual = new File(".");
+//		String[] lista_ficheros_dir_actual = directorio_actual.list();
+//		
+//		if (lista_ficheros_dir_actual != null)
+//		{
+//			String nombre = directorio_actual.getName();
+//			listarDirectorio(nombre);
+//		}
+//		else
+//			System.out.println("No hay ficheros en el directorio.");
 		
 		/**
 		 * Hacer un programa que dada una ruta, muestre los subdirectorios ocultos.
 		 */
-//		File directorios_ocultos = new File ("C:/Users/Administrador.000/IsabelGrupoTarde/FicherosJava");
-//		String[] lista_ficheros_ocultos = directorios_ocultos.list();
-//		
-//		if (lista_ficheros_ocultos != null)
-//			for(int i = 0; i < lista_ficheros_ocultos.length; i++)
-//			{
-//				File sub_directorio = new File (lista_ficheros_ocultos[i]);
-//				
-//				if((sub_directorio.isDirectory()) && (lista_ficheros_ocultos[i].charAt(0)=='.'))
-//				{
-//					System.out.println("Ocultos: " +lista_ficheros_ocultos[i]);
-//				}
-//				else
-//					System.out.println("No ocultos: " +lista_ficheros_ocultos[i]);
-//			}
-//			
-//		else
-//			System.out.println("No hay ficheros ni directorios en el directorio.");
-//		
+		String ruta = "C:/Users/Administrador.000/IsabelGrupoTarde/FicherosJava";
+		File directorios_ocultos = new File (ruta);
+		String[] lista_ficheros_ocultos = directorios_ocultos.list();
+		
+		if (lista_ficheros_ocultos != null)
+			for(int i = 0; i < lista_ficheros_ocultos.length; i++)
+			{
+				File sub_directorio = new File (ruta+"\\"+lista_ficheros_ocultos[i]);
+				
+				if((sub_directorio.isDirectory()) && (lista_ficheros_ocultos[i].charAt(0)=='.'))
+				{
+					System.out.println("Ocultos: " +lista_ficheros_ocultos[i]);
+				}
+			}
+			
+		else
+			System.out.println("No hay ficheros ni directorios en el directorio.");
+		
 		
 		
 	}
